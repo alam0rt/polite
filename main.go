dc1f041eb32151049a956c04c13c101b536bcdd9
alam0rt <samlockart@gmail.com>
2019-10-13 16:12:13 +1100

    easier to read

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net"
 	"os/exec"
 	"strings"
@@ -11,32 +13,65 @@ import (
 	"net/http"
 )
 
+// NAME is the name of the program
+const NAME = "polite"
+
 // Message is the type which we recieve from
 // clients
 type Message struct {
 	Bool bool
 }
 
+// Host represents a remote client
+type Host struct {
+	remoteAddr string
+	remotePort string
+	request    Message
+	ready      bool
+}
+
 // arrayFlags holds a slice of flags
 type arrayFlags []string
 
+// String satisfies the Value interface used by flags
 func (i *arrayFlags) String() string {
 	return "string"
 }
 
+// Set satisfies the Value interface used by flags
 func (i *arrayFlags) Set(value string) error {
 	*i = append(*i, strings.TrimSpace(value))
 	return nil
 }
 
+// HostFlags holds the passed in host flags
+// e.g. -host grafana -host mongodb.default.svc.cluster.local
 var HostFlags arrayFlags
 
 // ExecFlag is a flag which when set will be exeuted when pods are running
 var ExecFlag = flag.String("exec", "/bin/true", "help message for flagname")
+
+// ListenPort is a flag to set which port to listen on
 var ListenPort = flag.String("port", "8080", "port to listen on")
 
 // Incoming will save our incoming requests
 var Incoming map[string]bool
+
+// Hosts will hold the Host struct
+var Hosts map[string]Host
+
+func (h *Host) status() bool {
+	return h.ready
+}
+
+func (h *Host) resolve() {
+	if h.remoteAddr == "" {
+		fmt.Print("bad")
+	}
+	resolved := resolveHost(h.remoteAddr)
+	fmt.Print(resolved)
+
+}
 
 func resolveHost(ip string) []string {
 	resolved, err := net.LookupAddr(ip)
@@ -44,6 +79,17 @@ func resolveHost(ip string) []string {
 		fmt.Printf("%s cannot be resolved!", ip)
 	}
 	return resolved
+}
+
+func matchHost2(resolved []string) (string, bool) {
+	for _, r := range resolved {
+		for h := range Hosts {
+			if h == r {
+				return h, true
+			}
+		}
+	}
+	return "", false
 }
 
 func matchHost(resolved []string) (string, bool) {
@@ -57,6 +103,20 @@ func matchHost(resolved []string) (string, bool) {
 	return "", false
 }
 
+func checkHosts2() bool {
+	// assume everoyne is unready
+	unready := len(Hosts)
+	for _, h := range Hosts {
+		if h.ready {
+			// if the host is ready we can subtract from our count
+			unready--
+		}
+	}
+	if unready == 0 {
+		return true
+	}
+	return false
+}
 func checkHosts() bool {
 	for _, h := range Incoming {
 		if h == false {
@@ -66,35 +126,50 @@ func checkHosts() bool {
 	return true
 }
 
+func countReady() int {
+	count := 0
+	for host := range Hosts {
+		if Hosts[host].ready {
+			count++
+		}
+	}
+	return count
+}
+
 func handleMessage(r *http.Request, message *Message) {
 	// handleMessage takes an incoming request and determines if it satisfies our conditions
 	raddr := r.RemoteAddr
-	host, _, err := net.SplitHostPort(raddr)
+	ip, port, err := net.SplitHostPort(raddr)
 	if err != nil {
 		fmt.Printf("%s not a valid host?", raddr)
 	}
 
-	resolved := resolveHost(host)
-	matchedHost, isMatch := matchHost(resolved)
+	resolved := resolveHost(ip)
+	name, isMatch := matchHost2(resolved)
 	if isMatch {
-		Incoming[matchedHost] = true
+		// if the host is a match let's fill out the struct with
+		// the rest of the info
+		Hosts[name] = Host{
+			remoteAddr: ip,
+			remotePort: port,
+			ready:      true,
+			request:    *message,
+		}
 	} else {
 		fmt.Printf("%s not in list of hosts\n", resolved[0])
 	}
-	if checkHosts() {
-		// everyone has phone in so now
-		// we can exec
+	if checkHosts2() {
+		// everyone has phone in so now we can exec
 		fmt.Printf("All %v host(s) have phoned in...\n", len(Incoming))
 		runCommand()
-		// Should cleanly exit now and pass execution onto target program
 	} else {
 		i := 0
-		for _, v := range Incoming {
-			if v == true {
+		for _, v := range Hosts {
+			if v.ready == true {
 				i++
 			}
 		}
-		fmt.Printf("%v/%v\n", i, len(Incoming))
+		fmt.Printf("%v/%v\n", i, len(Hosts))
 	}
 
 }
@@ -108,22 +183,38 @@ func runCommand() {
 }
 
 func main() {
+	var (
+		buf    bytes.Buffer
+		logger = log.New(&buf, "logger: ", log.Lshortfile)
+	)
+
+	logger.Printf("started %s on :%s\n", NAME, *ListenPort)
+	logger.Printf("politely waiting to execute %s", *ExecFlag)
+
+	fmt.Print(&buf)
 	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
 		var message Message
 		json.NewDecoder(r.Body).Decode(&message)
 		handleMessage(r, &message)
-		fmt.Fprintf(w, "%s", "pong")
+		fmt.Fprintf(w, "%s\n", "pong")
+		for h := range Hosts {
+			if Hosts[h].ready {
+				logger.Printf("%s ready (%v/%v)\n", h, countReady(), len(Hosts))
+			}
+		}
+		fmt.Print(&buf)
 	})
 	http.ListenAndServe(":"+*ListenPort, nil)
 }
 
 func init() {
-	// create a map with all the incoming clients
 	Incoming = make(map[string]bool)
+	Hosts = make(map[string]Host)
 	flag.Var(&HostFlags, "host", "hosts to wait for")
 	flag.Parse()
 
 	for _, host := range HostFlags {
 		Incoming[host] = false
+		Hosts[host] = Host{}
 	}
 }
